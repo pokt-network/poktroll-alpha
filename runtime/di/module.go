a20bcb8b4fb6933337401a58c264eb749c39c76e
Bryan White <bryanchriswhite@gmail.com>
2023-09-08 10:11:43 +0200

    fixup: simplify DI

--- runtime/di/module.go
+++ runtime/di/module.go
@@ -5,15 +5,3 @@ type Module interface {
 	Start() error
 	CascadeStart() error
 }
-
-type ModuleInternals[Deps any] struct {
-	deps *Deps
-}
-
-func (m *ModuleInternals[Deps]) Deps() *Deps {
-	return m.deps
-}
-
-func (m *ModuleInternals[Deps]) ResolveDeps(deps *Deps) {
-	m.deps = deps
-}
