e3827bb436a4ae852c224b76e2fbf422f287a915
Daniel Olshansky <olshansky.daniel@gmail.com>
2023-09-13 22:06:11 -0700

    Deleted existing session data

--- x/poktroll/client/cli/query.go
+++ x/poktroll/client/cli/query.go
@@ -25,7 +25,6 @@ func GetQueryCmd(queryRoute string) *cobra.Command {
 	}
 
 	cmd.AddCommand(CmdQueryParams())
-	cmd.AddCommand(CmdGetSession())
 	// this line is used by starport scaffolding # 1
 
 	return cmd
