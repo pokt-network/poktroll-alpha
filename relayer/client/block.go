3c7b53c4663028a7543373cbf637c4a122966550
Bryan White <bryanchriswhite@gmail.com>
2023-09-19 08:53:30 +0200

    fix: restructure `tendermintBlockEvent` to match

--- relayer/client/block.go
+++ relayer/client/block.go
@@ -3,29 +3,20 @@ package client
 import (
 	"encoding/hex"
 	"encoding/json"
-	"strconv"
-
 	"poktroll/x/servicer/types"
 )
 
 type tendermintBlockEvent struct {
-	Result tendermintBlockEventData `json:"result"`
+	Block  tendermintBlock `json:"block"`
 	height uint64
 	hash   []byte
 }
 
-type tendermintBlockEventData struct {
-	Data struct {
-		Value struct {
-			Block struct {
-				Header struct {
-					Height         string `json:"height"`
-					LastCommitHash string `json:"last_commit_hash"`
-				} `json:"header"`
-			} `json:"block"`
-		} `json:"value"`
-		Type string `json:"type"`
-	} `json:"data"`
+type tendermintBlock struct {
+	Header struct {
+		Height         uint64 `json:"height"`
+		LastCommitHash string `json:"last_commit_hash"`
+	} `json:"header"`
 }
 
 func (blockEvent *tendermintBlockEvent) Height() uint64 {
@@ -40,16 +31,12 @@ func NewTendermintBlockEvent(blockEventMessage []byte) (_ types.Block, err error
 	blockEvent := new(tendermintBlockEvent)
 	json.Unmarshal(blockEventMessage, blockEvent)
 
-	if blockEvent.Result == (tendermintBlockEventData{}) {
+	if blockEvent.Block == (tendermintBlock{}) {
 		return nil, nil
 	}
 
-	blockEvent.height, err = strconv.ParseUint(blockEvent.Result.Data.Value.Block.Header.Height, 10, 64)
-	if err != nil {
-		return nil, err
-	}
-
-	blockEvent.hash, err = hex.DecodeString(blockEvent.Result.Data.Value.Block.Header.LastCommitHash)
+	blockEvent.height = blockEvent.Block.Header.Height
+	blockEvent.hash, err = hex.DecodeString(blockEvent.Block.Header.LastCommitHash)
 	if err != nil {
 		return nil, err
 	}
