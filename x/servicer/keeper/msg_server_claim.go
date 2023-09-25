be84c792d81de0a3eca52a8e1135e6ef937ea3dd
Bryan White <bryanchriswhite@gmail.com>
2023-09-25 23:14:27 +0200

    chore: add TODOs for `MsgClaim` handling validation

--- x/servicer/keeper/msg_server_claim.go
+++ x/servicer/keeper/msg_server_claim.go
@@ -11,6 +11,13 @@ func (k msgServer) Claim(goCtx context.Context, msg *types.MsgClaim) (*types.Msg
 	ctx := sdk.UnwrapSDKContext(goCtx)
 	//logger := k.Logger(ctx).With("method", "Claim")
 
+	// INCOMPLETE: validate that the message is signed by the servicer address
+	// in the claim.
+	//
+	// CONSIDERATION: use `msg.GetSigners()` or the `cosmos.msg.v1.signer`
+	// option on `MsgClaim` protobuf type.
+	// (see: https://github.com/cosmos/cosmos-sdk/blob/main/proto/cosmos/bank/v1beta1/bank.proto#L34C1-L35C1)
+
 	if err := k.InsertClaim(ctx, msg); err != nil {
 		return nil, err
 	}
